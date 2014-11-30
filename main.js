529aa1c31eb8f3825f67a42ae21a67560bd774b8
Yusuke Shinyama <yusuke@shinyama.jp>
2014-11-30 19:26:57 +0900

    Basic platformer stuff.

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 // main.js
 
+function Scene(width, height)
+{
+  this.floor = 32;
+  this.rect = rect_make(0, height-this.floor, width, this.floor);
+}
+Scene.prototype.collide = function (rect, vx, vy)
+{
+  return collideRect(this.rect, rect, pt_make(vx, vy));
+}
+
+function Player(scene, width, height)
+{
+  this.speed = 8;
+  this.gravity = 2;
+  this.maxspeed = 16;
+  this.jumpacc = -16;
+  this.scene = scene;
+  this.rect = rect_make(0, 0, width, height);
+  this.vx = this.vy = 0;
+  this.gy = 0;
+}
+Player.prototype.idle = function()
+{
+  var v = pt_make(this.speed * this.vx, this.gy);
+  var d = this.scene.collide(this.rect, v.x, v.y);
+  d.x = this.scene.collide(this.rect, v.x, d.y).x;
+  d.y = this.scene.collide(this.rect, d.x, v.y).y;
+  this.rect.x += d.x;
+  this.rect.y += d.y;
+  this.gy = Math.min(d.y + this.gravity, this.maxspeed);
+}
+Player.prototype.jump = function()
+{
+  var v = this.scene.collide(this.rect, 0, this.gy);
+  if (0 < this.gy && v.y == 0) {
+    this.gy = this.jumpacc;
+    return true;
+  }
+  return false;
+}
+
 function Game(canvas, image, audio)
 {
   this.canvas = canvas;
@@ -10,8 +51,8 @@ function Game(canvas, image, audio)
 
 Game.prototype.init = function()
 {
-  this.x = this.y = 0;
-  this.vx = this.vy = 0;
+  this.scene = new Scene(this.canvas.width, this.canvas.height);
+  this.player = new Player(this.scene, 32, 32);
 }
 
 Game.prototype.keydown = function(ev)
@@ -20,22 +61,22 @@ Game.prototype.keydown = function(ev)
   case 37:			// LEFT
   case 65:			// A
   case 72:			// H
-    this.vx = -1;
+    this.player.vx = -1;
     break;
   case 39:			// RIGHT
   case 68:			// D
   case 76:			// L
-    this.vx = +1;
+    this.player.vx = +1;
     break;
   case 38:			// UP
   case 87:			// W
   case 75:			// K
-    this.vy = -1;
+    this.player.vy = -1;
     break;
   case 40:			// DOWN
   case 83:			// S
   case 74:			// J
-    this.vy = +1;
+    this.player.vy = +1;
     break;
   case 13:			// ENTER
   case 32:			// SPACE
@@ -57,7 +98,7 @@ Game.prototype.keyup = function(ev)
   case 39:			// RIGHT
   case 68:			// D
   case 76:			// L
-    this.vx = 0;
+    this.player.vx = 0;
     break;
   case 38:			// UP
   case 87:			// W
@@ -65,30 +106,32 @@ Game.prototype.keyup = function(ev)
   case 40:			// DOWN
   case 83:			// S
   case 74:			// J
-    this.vy = 0;
+    this.player.vy = 0;
     break;
   }
 }
 
 Game.prototype.idle = function()
 {
+  this.player.idle();
   this.repaint();
-  this.x += this.vx; this.y += this.vy;
 }
 
 Game.prototype.repaint = function()
 {
   this.ctx.clearRect(0, 0, canvas.width, canvas.height);
   this.ctx.save();
-  this.ctx.fillStyle = 'blue';
-  this.ctx.fillRect(this.x, this.y, 100, 100);
-  this.ctx.drawImage(this.image, this.x, this.y);
+  //this.ctx.fillStyle = 'blue';
+  //this.ctx.fillRect(this.x, this.y, 100, 100);
+  this.ctx.drawImage(this.image, this.player.rect.x, this.player.rect.y);
   this.ctx.restore();
 }
 
 Game.prototype.action = function()
 {
-  this.audio.play();
+  if (this.player.jump()) {
+    this.audio.play();
+  }
 }
 
 function run()
---
+++ utils.js
@@ -0,0 +1,133 @@
+// utils.js
+
+// log(x)
+function log(x)
+{
+  if (window.console != 'undefined') {
+    window.console.log(x);
+  }
+}
+
+// clamp(v0, v, v1)
+function clamp(v0, v, v1)
+{
+  return Math.min(Math.max(v, v0), v1);
+}
+  
+// rnd(n)
+function rnd(a, b=0)
+{
+  if (b < a) {
+    var c = a;
+    a = b;
+    b = c;
+  }
+  return Math.floor(Math.random()*(b-a))+a;
+}
+
+// format
+function format(v, n=3, c=" ")
+{
+  var s = "";
+  while (s.length < n) {
+    s = (v % 10)+s;
+    v /= 10;
+    if (v <= 0) break;
+  }
+  while (s.length < n) {
+    s = c+s;
+  }
+  return s;
+}
+
+// Point
+function pt_make(x, y)
+{
+  return {x:x, y:y};
+}
+function pt_move(p, dx, dy)
+{
+  return {x:(p.x+dx), y:(p.y+dy)};
+}
+function pt_str(p)
+{
+  return "("+p.x+", "+p.y+")";
+}
+
+// Rectangle
+function rect_make(x, y, width, height)
+{
+  return {x:x, y:y, width:width, height:height};
+}
+function rect_move(rect, dx, dy)
+{
+  return {x:(rect.x+dx), y:(rect.y+dy), width:rect.width, height:rect.height};
+}
+function rect_str(rect)
+{
+  return "("+rect.x+", "+rect.y+", "+rect.width+", "+rect.height+")";
+}
+
+// collideRect
+function collideHLine(x0, x1, y, rect, v)
+{
+  var left = rect.x;
+  var right = rect.x+rect.width;
+  var top = rect.y;
+  var bottom = rect.y+rect.height;
+  var dy;
+  if (y <= top && top < y+v.y) {
+    dy = top - y;
+  } else if (bottom <= y && y+v.y < bottom) {
+    dy = bottom - y;
+  } else {
+    return v;
+  }
+  // assert(v.y != 0);
+  var dx = Math.floor(v.x*dy / v.y);
+  if ((v.x <= 0 && x1+dx <= left) ||
+      (0 <= v.x && right <= x0+dx) ||
+      (x1+dx < left || right < x0+dx)) {
+    return v;
+  }
+  return pt_make(dx, dy);
+}
+
+function collideVLine(y0, y1, x, rect, v)
+{
+  var left = rect.x;
+  var right = rect.x+rect.width;
+  var top = rect.y;
+  var bottom = rect.y+rect.height;
+  var dx;
+  if (x <= left && left < x+v.x) {
+    dx = left - x;
+  } else if (right <= x && x+v.x < right) {
+    dx = right - x;
+  } else {
+    return v;
+  }
+  // assert(v.x != 0);
+  var dy = Math.floor(v.y*dx / v.x);
+  if ((v.y <= 0 && y1+dy <= top) ||
+      (0 <= v.y && bottom <= y0+dy) ||
+      (y1+dy < top || bottom < y0+dy)) {
+    return v;
+  }
+  return pt_make(dx, dy);
+}
+
+function collideRect(r0, r1, v)
+{
+  if (0 < v.x) {
+    v = collideVLine(r1.y, r1.y+r1.height, r1.x+r1.width, r0, v);
+  } else if (v.x < 0) {
+    v = collideVLine(r1.y, r1.y+r1.height, r1.x, r0, v);
+  }
+  if (0 < v.y) {
+    v = collideHLine(r1.x, r1.x+r1.width, r1.y+r1.height, r0, v);
+  } else if (v.y < 0) {
+    v = collideHLine(r1.x, r1.x+r1.width, r1.y, r0, v);
+  }
+  return v;
+}
