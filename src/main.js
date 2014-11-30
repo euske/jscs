48d2c50da5834519d14301c339eef61ba97afd7b
Yusuke Shinyama <yusuke@shinyama.jp>
2014-11-30 19:42:29 +0900

    Tiled background.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
 // main.js
 
-function Scene(width, height)
+function Scene(sprites, ctx, width, height)
 {
-  this.floor = 32;
-  this.rect = rect_make(0, height-this.floor, width, this.floor);
+  this.tilesize = 32;
+  this.sprites = sprites;
+  this.ctx = ctx;
+  this.floor = rect_make(0, height-this.tilesize, width, this.tilesize);
 }
 Scene.prototype.collide = function (rect, vx, vy)
 {
-  return collideRect(this.rect, rect, pt_make(vx, vy));
+  return collideRect(this.floor, rect, pt_make(vx, vy));
+}
+Scene.prototype.repaint = function()
+{
+  for (var x = 0; x < this.floor.width; x += this.tilesize) {
+    this.ctx.drawImage(this.sprites,
+		       this.tilesize, 0, this.tilesize, this.tilesize,
+		       x, this.floor.y,
+		       this.tilesize, this.tilesize);
+  }
 }
 
-function Player(scene, width, height)
+function Player(sprites, ctx, scene, width, height)
 {
   this.speed = 8;
   this.gravity = 2;
   this.maxspeed = 16;
   this.jumpacc = -16;
+  this.sprites = sprites;
+  this.ctx = ctx;
   this.scene = scene;
   this.rect = rect_make(0, 0, width, height);
   this.vx = this.vy = 0;
@@ -40,19 +53,26 @@ Player.prototype.jump = function()
   }
   return false;
 }
+Player.prototype.repaint = function()
+{
+  this.ctx.drawImage(this.sprites,
+		     0, 0, this.rect.width, this.rect.height,
+		     this.rect.x, this.rect.y,
+		     this.rect.width, this.rect.height);
+}
 
-function Game(canvas, image, audio)
+function Game(canvas, sprites, audio)
 {
   this.canvas = canvas;
   this.ctx = canvas.getContext('2d');
-  this.image = image;
+  this.sprites = sprites;
   this.audio = audio;
 }
 
 Game.prototype.init = function()
 {
-  this.scene = new Scene(this.canvas.width, this.canvas.height);
-  this.player = new Player(this.scene, 32, 32);
+  this.scene = new Scene(this.sprites, this.ctx, this.canvas.width, this.canvas.height);
+  this.player = new Player(this.sprites, this.ctx, this.scene, 32, 32);
 }
 
 Game.prototype.keydown = function(ev)
@@ -123,7 +143,8 @@ Game.prototype.repaint = function()
   this.ctx.save();
   //this.ctx.fillStyle = 'blue';
   //this.ctx.fillRect(this.x, this.y, 100, 100);
-  this.ctx.drawImage(this.image, this.player.rect.x, this.player.rect.y);
+  this.scene.repaint();
+  this.player.repaint();
   this.ctx.restore();
 }
 
@@ -138,9 +159,9 @@ function run()
 {
   var dt = 1000/20;
   var canvas = document.getElementById('canvas');
-  var image = document.getElementById('image');
+  var sprites = document.getElementById('sprites');
   var audio = document.getElementById('audio');
-  var game = new Game(canvas, image, audio);
+  var game = new Game(canvas, sprites, audio);
   var idle = function() { game.idle(); window.setTimeout(idle, dt); };
   var keydown = function(e) { game.keydown(e); };
   var keyup = function(e) { game.keyup(e); };
