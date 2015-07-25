84c08ce33a05df94650993671a600ade841350ce
Yusuke Shinyama <yusuke@shinyama.jp>
2015-07-26 01:31:00 +0900

    Improved tooling.

--- tools/copynew.sh
+++ tools/copynew.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 dst="$1"
-rsync -v -a ./ "$dst" --exclude '.*'
-cp .gitignore .rsync "$dst"
+name=${dst/*\//}
+[ -d "$dst" ] || exit 1
+rsync -v -a --exclude '*.md' --exclude '*.wav' --exclude 'tmp' --exclude 'tools' --exclude '.*' ./ "$dst"
+cp .gitignore "$dst"
+echo "--exclude *.wav --exclude *.md --exclude tmp --exclude tools --exclude Makefile tabesugi:public/file/ludumdare.tabesugi.net/$name/" > "$dst"/.rsync
+sed -e "s/@@NAME@@/$name/g" tools/template.html > "$dst"/index.html
