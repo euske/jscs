c653209304cbe36d372abaa4e7763c414bab911a
Yusuke Shinyama <yusuke@shinyama.jp>
2015-11-01 09:08:18 +0900

    Fixed: assets

--- tools/copynew.sh
+++ tools/copynew.sh
@@ -2,7 +2,11 @@
 dst="$1"
 name=${dst/*\//}
 [ -d "$dst" ] || exit 1
-rsync -v -a --exclude '*.md' --exclude '*.wav' --exclude 'tmp' --exclude 'tools' --exclude '.*' ./ "$dst"
-cp .gitignore "$dst"
 echo "--exclude *.wav --exclude *.md --exclude tmp --exclude tools --exclude Makefile tabesugi:public/file/ludumdare.tabesugi.net/$name/" > "$dst"/.rsync
+mkdir "$dst"/src
+mkdir "$dst"/assets
+cp .gitignore "$dst"
+cp src/*.js "$dst"/src
+cp assets/*.png "$dst"/assets
+cp assets/*.mp3 "$dst"/assets
 sed -e "s/@@NAME@@/$name/g" tools/template.html > "$dst"/index.html
