4a660c443911939c8563565a1ab4068d917117c5
Yusuke Shinyama <yusuke@shinyama.jp>
2015-11-27 22:15:47 +0900

    Changed: sample code.

--- src/game.js
+++ src/game.js
@@ -14,38 +14,37 @@ function Player(bounds)
   this._jumpt = -1;
 }
 
-Player.prototype = Object.create(Actor.prototype);
-
-Player.prototype.jump = function (jumping)
-{
-  if (jumping) {
-    if (this._landed) {
-      this._jumpt = 0;
-      this.velocity.y = this.jumpacc;
+define(Player, Actor, 'Actor', {
+  jump: function (jumping) {
+    if (jumping) {
+      if (this._landed) {
+	this._jumpt = 0;
+	this.velocity.y = this.jumpacc;
+      }
+    } else {
+      this._jumpt = -1;
     }
-  } else {
-    this._jumpt = -1;
-  }
-};
+  },
 
-Player.prototype.usermove = function (v)
-{
-  this.velocity.x = v.x*this.speed;
-}
+  usermove: function (v) {
+    this.velocity.x = v.x*this.speed;
+  },
 
-Player.prototype.update = function ()
-{
-  if (0 <= this._jumpt && this._jumpt < this.maxacctime) {
-    this._jumpt++;
-    this.velocity.y -= this.gravity;
-  }
-  this.velocity.y += this.gravity;
-  this.velocity.y = clamp(-this.maxspeed, this.velocity.y, this.maxspeed);
-  var v = this.hitbox.contact(this.velocity, this.scene.ground);
-  this._landed = (0 < this.velocity.y && v.y === 0);
-  this.velocity = v;
-  this.move(this.velocity.x, this.velocity.y);
-};
+  update: function () {
+    this._Actor_update();
+    if (0 <= this._jumpt && this._jumpt < this.maxacctime) {
+      this._jumpt++;
+      this.velocity.y -= this.gravity;
+    }
+    this.velocity.y += this.gravity;
+    this.velocity.y = clamp(-this.maxspeed, this.velocity.y, this.maxspeed);
+    var v = this.hitbox.contact(this.velocity, this.scene.ground);
+    this._landed = (0 < this.velocity.y && v.y === 0);
+    this.velocity = v;
+    this.move(this.velocity.x, this.velocity.y);
+  },
+});
+
 
 
 //  Game
@@ -68,18 +67,28 @@ Game.prototype.init = function ()
   
   // show a banner.
   var scene = this;
-  var textbox = new TextBox(this.frame);
-  textbox.putText(app.font, ['GAME!!1'], 'center', 'center');
-  textbox.duration = app.framerate*2;
-  textbox.update = function () {
-    TextBox.prototype.update.call(textbox);
-    textbox.visible = blink(scene.ticks, app.framerate/2);
+  var tbox = new TextBox(this.frame);
+  tbox.putText(app.font, ['GAME!!1'], 'center', 'center');
+  tbox.duration = app.framerate*2;
+  tbox.update = function () {
+    TextBox.prototype.update.call(tbox);
+    tbox.visible = blink(scene.ticks, app.framerate/2);
   };
-  this.addObject(textbox);
+  this.addObject(tbox);
 
-  var tt = new TextBoxTT(new Rectangle(10, 10, 200, 100));
-  tt.addTask(app.font, 'THIS IS GAEM.\nYES IT IS.', 4, app.audios.beep);
-  this.addObject(tt);
+  this.textbox = new TextBoxTT(new Rectangle(10, 10, 200, 100));
+  this.textbox.addDisplay(app.font, 'THIS IS GAEM.\nYES IT IS.', 4, app.audios.beep);
+  this.textbox.blinking = 10;
+  var menu = this.textbox.addMenu(app.font);
+  menu.sound = app.audios.beep;
+  menu.addItem(new Vec2(10,50), 'AAA');
+  menu.addItem(new Vec2(20,60), 'BB');
+  menu.addItem(new Vec2(30,70), 'CCCC');
+  menu.selected.subscribe(function (obj, value) {
+    console.log("selected:"+value);
+    scene.textbox.visible = false;
+  });
+  this.addObject(this.textbox);
 };
 
 Game.prototype.render = function (ctx, bx, by)
@@ -90,14 +99,26 @@ Game.prototype.render = function (ctx, bx, by)
   GameScene.prototype.render.call(this, ctx, bx, by);
 };
 
+Game.prototype.keydown = function (key)
+{
+  GameScene.prototype.keydown.call(this, key);
+  if (this.textbox.visible) {
+    this.textbox.keydown(key);
+  }
+};
+
 Game.prototype.update = function ()
 {
   GameScene.prototype.update.call(this);
-  this.player.usermove(this.app.key_dir);
+  if (!this.textbox.visible) {
+    this.player.usermove(this.app.key_dir);
+  }
 };
 
 Game.prototype.set_action = function (action)
 {
   GameScene.prototype.set_action(this, action);
-  this.player.jump(action);
+  if (!this.textbox.visible) {
+    this.player.jump(action);
+  }
 }
